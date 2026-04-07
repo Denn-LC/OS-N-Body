0b063c0063b9b871a90c6b406c231cc9629cb3c4
Dennison Leadbetter-Clarke <dennisonleadbetterclarke@gmail.com>
2026-04-07 19:20:02 +0100

    added energy checks to ensure system obeys 1st law of thermodynamics within 1% error

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
-# include <stdio.h>
-# include <math.h>
+#include <math.h>
+#include <stdio.h>
 
-#define G   1.0 // gravitational constant, is 6.67*10^-11 N*m^2/kg^2 in reality
-#define DT  0.01 // timestep
-#define STEPS 1000 // total number of calculations performed per run
+#define G 1.0         // gravitational constant, is 6.67*10^-11 N*m^2/kg^2 in reality
+#define DT 0.01       // timestep
+#define STEPS 1000    // total number of calculations performed per run
 #define SOFTENING 0.1 // so forces dont blow up at dist = 0 to infinity
 
 int main(void) {
@@ -17,20 +17,40 @@ int main(void) {
     double vx2 = 0.0, vy2 = -0.5;
     double m2 = 1.0;
 
-    int i;
-    for (i = 0; i < STEPS; i++) {
+    double E_initial = 0.0;
+
+    for (int i = 0; i < STEPS; i++) {
 
         double dx = x2 - x1;
         double dy = y2 - y1;
-        
-        double dist = sqrt(dx*dx + dy*dy + SOFTENING*SOFTENING);
+
+        double dist = sqrt(dx * dx + dy * dy + SOFTENING * SOFTENING);
 
         double force = G * m1 * m2 / (dist * dist);
 
-        double ax1 =  force * dx / dist / m1;
-        double ay1 =  force * dy / dist / m1;
+        double ax1 = force * dx / dist / m1;
+        double ay1 = force * dy / dist / m1;
         double ax2 = -force * dx / dist / m2;
         double ay2 = -force * dy / dist / m2;
+
+        // total kinetic energy of system = (1/2)*(m)*(v^2) note, velocity is the total magnitude of
+        // components
+        double ke = (0.5 * m1 * (vx1 * vx1 + vy1 * vy1)) + (0.5 * m2 * (vx2 * vx2 + vy2 * vy2));
+        // total potential energy of the system = -(G*M*m)/r note, this is hardcoded for 2 bodies
+        double pe = -G * m1 * m2 / dist;
+        // total energy of the system
+        double energy = ke + pe;
+
+        if (i == 0) {
+            E_initial = energy;
+        }
+
+        double drift = energy - E_initial;
+        // check if energy is being conserved
+        if (fabs(drift) > 0.01 * fabs(E_initial)) { // if drift is bigger the  1% of the inital
+            printf("Energy is not being conserved");
+            break;
+        }
 
         vx1 += ax1 * DT;
         vy1 += ay1 * DT;
@@ -42,8 +62,7 @@ int main(void) {
         x2 += vx2 * DT;
         y2 += vy2 * DT;
 
-        printf("step %4d  body1=(%.3f, %.3f)  body2=(%.3f, %.3f)\n",
-               i, x1, y1, x2, y2);
+        printf("step %4d  body1=(%.3f, %.3f)  body2=(%.3f, %.3f)\n", i, x1, y1, x2, y2);
     }
     return 0;
 }
